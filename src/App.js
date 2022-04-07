33937ef5eae5b60abfd4c47cacbe5f46695de599
MAC <user@MACs-MacBook-Pro.local>
2022-04-07 21:38:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fuck any other language than React Js
         </a>
       </header>
     </div>
